100eaa04a6e0b13c8718cfc33c597bc6b8cb1e73
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-12-02 13:11:45 +0300

    Tool: v2

--- plugin/Tool.h
+++ plugin/Tool.h
@@ -9,28 +9,39 @@
 
 namespace plugin
 {
-    struct ToolI: public Interface
+    struct ToolI
     {
         virtual const Texture *get_icon() = 0;
+        virtual const char    *get_name() = 0;
 
-        virtual void paint_on_press  (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context, Color color) = 0;
-        virtual void paint_on_release(RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context, Color color) = 0;
-        virtual void paint_on_move   (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context, Color color) = 0;
-        virtual void deactivate      (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context, Color color) = 0;
+        virtual void paint_on_press  (RenderTargetI &perm, RenderTargetI &temp, const MouseContext &context, const MouseButton &pressed , const Color &color) = 0;
+        virtual void paint_on_release(RenderTargetI &perm, RenderTargetI &temp, const MouseContext &context, const MouseButton &released, const Color &color) = 0;
+        virtual void paint_on_move   (RenderTargetI &perm, RenderTargetI &temp, const MouseContext &context, const vec2d       &offset  , const Color &color) = 0;
+        virtual void deactivate      (RenderTargetI &perm, RenderTargetI &temp,                                                           const Color &color) = 0;
     };
+
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    class Tool: public ToolI
+    {};
 
     struct ToolManagerI
     {
-        virtual void   set_color(Color color) = 0;
-        virtual void   set_tool (ToolI *tool) = 0;
+        virtual void   set_color(const Color &color) = 0;
+        virtual void   set_tool (const Tool  *tool ) = 0;
 
-        virtual ToolI *get_tool ()            = 0;
-        virtual Color  get_color()            = 0;
+        virtual       Tool  *get_tool () const = 0;
+        virtual const Color &get_color() const = 0;
 
-        virtual void paint_on_move   (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context) = 0;
-        virtual void paint_on_press  (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context) = 0;
-        virtual void paint_on_release(RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context) = 0;
-        virtual void disable_tool    (RenderTargetI *perm, RenderTargetI *temp, MouseContext local_context) = 0;
+        virtual void paint_on_move   (RenderTarget &perm, RenderTarget &temp, const MouseContext &context, const vec2d       &offset)   = 0;
+        virtual void paint_on_press  (RenderTarget &perm, RenderTarget &temp, const MouseContext &context, const MouseButton &pressed)  = 0;
+        virtual void paint_on_release(RenderTarget &perm, RenderTarget &temp, const MouseContext &context, const MouseButton &released) = 0;
+        virtual void disable_tool    (RenderTarget &perm, RenderTarget &temp) = 0;
+    };
+
+    struct ToolCreateI
+    {
+        virtual Tool *create_tool(ToolI &toolI) = 0;
     };
 }
 
